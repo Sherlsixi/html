2da6f9139d0d1e52cc92b24c6ee1919b1e4a5e5b
Xiao Xiao <sixihana@gmail.com>
2023-11-10 19:32:27 +0900

    swiper

---
+++ 2311_javascript/231110_mobile/京东移动端首页 swiper使用练习/js/index.js
@@ -0,0 +1,18 @@
+window.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+});
