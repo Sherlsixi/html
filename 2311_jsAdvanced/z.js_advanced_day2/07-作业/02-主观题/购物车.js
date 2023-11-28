db2eded0171646b6ae61df8b798c590a8f276653
Xiao Xiao <sixihana@gmail.com>
2023-11-29 00:14:23 +0900

    advanced_day3

---
+++ 2311_jsAdvanced/z.js_advanced_day2/07-作业/02-主观题/购物车.js
@@ -0,0 +1,37 @@
+const dataArr = [
+  {
+    id: 1,
+    icon: "http://autumnfish.cn/static/火龙果.png",
+    isChecked: true,
+    num: 2,
+    price: 6,
+  },
+  {
+    id: 2,
+    icon: "http://autumnfish.cn/static/荔枝.png",
+    isChecked: false,
+    num: 7,
+    price: 20,
+  },
+  {
+    id: 3,
+    icon: "http://autumnfish.cn/static/榴莲.png",
+    isChecked: false,
+    num: 3,
+    price: 40,
+  },
+  {
+    id: 4,
+    icon: "http://autumnfish.cn/static/鸭梨.png",
+    isChecked: true,
+    num: 10,
+    price: 3,
+  },
+  {
+    id: 5,
+    icon: "http://autumnfish.cn/static/樱桃.png",
+    isChecked: false,
+    num: 20,
+    price: 34,
+  },
+];
