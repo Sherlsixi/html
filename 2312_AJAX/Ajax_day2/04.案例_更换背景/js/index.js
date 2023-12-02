6322e3665592ec939bd4377834d37270199d952d
Xiao Xiao <sixihana@gmail.com>
2023-12-02 13:53:52 +0900

    new

--- 2312_AJAX/Ajax_day2/04.案例_更换背景/js/index.js
+++ 2312_AJAX/Ajax_day2/04.案例_更换背景/js/index.js
@@ -4,25 +4,25 @@
  *  2. 上传成功时，"保存"图片url网址
  *  3. 网页运行后，"获取"url网址使用
  * */
-document.querySelector('.bg-ipt').addEventListener('change', e => {
+document.querySelector(".bg-ipt").addEventListener("change", (e) => {
   // 1. 选择图片上传，设置body背景
-  console.log(e.target.files[0])
-  const fd = new FormData()
-  fd.append('img', e.target.files[0])
+  console.log(e.target.files[0]);
+  const fd = new FormData();
+  fd.append("img", e.target.files[0]);
   axios({
-    url: 'http://hmajax.itheima.net/api/uploadimg',
-    method: 'POST',
-    data: fd
-  }).then(result => {
-    const imgUrl = result.data.data.url
-    document.body.style.backgroundImage = `url(${imgUrl})`
+    url: "http://hmajax.itheima.net/api/uploadimg",
+    method: "POST",
+    data: fd,
+  }).then((result) => {
+    const imgUrl = result.data.data.url;
+    document.body.style.backgroundImage = `url(${imgUrl})`;
 
     // 2. 上传成功时，"保存"图片url网址
-    localStorage.setItem('bgImg', imgUrl)
-  })
-})
+    localStorage.setItem("bgImg", imgUrl);
+  });
+});
 
 // 3. 网页运行后，"获取"url网址使用
-const bgUrl = localStorage.getItem('bgImg')
-console.log(bgUrl)
-bgUrl && (document.body.style.backgroundImage = `url(${bgUrl})`)
+const bgUrl = localStorage.getItem("bgImg");
+console.log(bgUrl);
+bgUrl && (document.body.style.backgroundImage = `url(${bgUrl})`);
