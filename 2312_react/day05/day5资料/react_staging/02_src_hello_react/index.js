647b1658cead4f23be887ad8f86e88dde05d51a2
Xiao Xiao <sixihana@gmail.com>
2023-12-23 00:10:20 +0900

    react-day5

--- 2312_react/day05/day5资料/react_staging/02_src_hello_react/index.js
+++ 2312_react/day05/day5资料/react_staging/02_src_hello_react/index.js
@@ -1,9 +1,9 @@
 //引入react核心库
-import React from 'react'
+import React from "react";
 //引入ReactDOM
-import ReactDOM from 'react-dom'
+import ReactDOM from "react-dom";
 //引入App组件
-import App from './App'
+import App from "./App";
 
 //渲染App到页面
-ReactDOM.render(<App/>,document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById("root"));
