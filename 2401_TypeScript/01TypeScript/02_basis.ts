02584d5320c2faf33c14fa0a65f882b05163ff6e
Xiao Xiao <sixihana@gmail.com>
2024-01-18 12:17:11 +0900

    new

--- 2401_TypeScript/01TypeScript/02_basis.ts
+++ 2401_TypeScript/01TypeScript/02_basis.ts
@@ -19,6 +19,37 @@ type Summary = { name: string };
 type Detail = { name: string; age: number };
 const johnDetail: Detail = { name: "John", age: 18 };
 const summary: Summary = johnDetail;
+const arr: Array<Summary> = [{ name: "tom" }];
 
 const johnSummary: Summary = { name: "John" };
 // const detail: Detail = johnSummary;
+
+function fn<T>(arr: Array<T>): T {
+  return arr[1];
+}
+
+type Abc<T> = {
+  a: string;
+  b: T;
+};
+const obj: Abc<number> = {
+  a: "",
+  b: 1,
+};
+
+class Star<T> {
+  private name: T;
+  private age: number;
+
+  constructor(name: T, age: number) {
+    this.name = name;
+    this.age = age;
+  }
+
+  show() {
+    console.log(this.name, this.age);
+  }
+}
+
+const p1 = new Star<string>("ZS", 31);
+p1.show();
